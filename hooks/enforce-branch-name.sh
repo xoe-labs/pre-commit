cc1a5b8b2c4454cec488723c80f5d0439ae06ace
David Arnold <dar@xoe.solutions>
2019-07-03 01:12:14 +0200

    Fix

--- hooks/enforce-branch-name.sh
+++ hooks/enforce-branch-name.sh
@@ -5,11 +5,11 @@ local_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 valid_branch_regex="^((feature|bugfix|improvement|prerelease|release|hotfix)\/[a-z0-9._-]+|master|12.0|11.0|10.0|9.0|8.0|7.0|6.0|6.1|5.0)$"
 
-message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
+message="There is something wrong with your branch name. \nBranch names in this project must adhere to this contract: \n\t$valid_branch_regex. \n\Your commit will be rejected. Please rename your branch and try again."
 
 if [[ ! $local_branch =~ $valid_branch_regex ]]
 then
-    echo "$message"
+    echo -e "$message"
     exit 1
 fi
 
