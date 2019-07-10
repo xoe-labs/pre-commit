292f4354b1c3e354801fef8f1f1ef735bd3cc66b
David Arnold <dar@xoe.solutions>
2019-07-10 19:15:46 +0200

    Add 3 letter suffix for developer

--- hooks/enforce-branch-name.sh
+++ hooks/enforce-branch-name.sh
@@ -3,7 +3,7 @@ LC_ALL=C
 
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_branch_regex="^((feature|bugfix|improvement|prerelease|release|hotfix)\/[a-z0-9._-]+|master|12.0|11.0|10.0|9.0|8.0|7.0|6.0|6.1|5.0)$"
+valid_branch_regex="^((feature|bugfix|improvement|prerelease|release|hotfix)\/[a-z0-9._-]+-[a-z]{3}|master|12.0|11.0|10.0|9.0|8.0|7.0|6.0|6.1|5.0)$"
 
 message="There is something wrong with your branch name. \nBranch names in this project must adhere to this contract: \n\t$valid_branch_regex. \n\Your commit will be rejected. Please rename your branch and try again."
 
